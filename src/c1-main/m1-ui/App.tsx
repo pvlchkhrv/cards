d356e1853f265b2d7cb61667aea5a7ae0c5bf370
pvl <pvlchkhrv@gmail.com>
2021-05-28 12:39:52 +0300

    init: friday project

---
+++ src/c1-main/m1-ui/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    {/*  hashrouter/provider*/}
+      <>
+        {/*<Header/>*/}
+      </>
+    </div>
+  )
+}
+
+export default App;
