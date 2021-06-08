77a806dd94a63c42f820248cd4e89f5bf12116e1
BorisPanich <panich2303@gmail.com>
2021-06-08 07:20:40 +0300

    fixed app-reducer

--- src/c1-main/m2-bll/app-reducer.ts
+++ src/c1-main/m2-bll/app-reducer.ts
@@ -1,6 +1,7 @@
 const initialState: InitialStateType = {
     status: 'idle',
-    isError: false
+    isError: null,
+    initialized: false
 }
 
 export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
@@ -9,26 +10,32 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
             return {...state, status: action.status}
         case 'APP/SET-ERROR':
             return {...state, isError: action.isError}
+        case 'APP/SET-INITIALIZED':
+            return {...state, initialized: action.value}
         default:
             return {...state}
     }
 }
 
-export const setAppError = (isError: boolean) => ({ type: 'APP/SET-ERROR', isError } as const);
-export const setAppStatus = (status:  RequestStatusType) => ({ type: 'APP/SET-STATUS', status } as const);
+export const setAppError = (isError: RequestErrorType) => ({type: 'APP/SET-ERROR', isError} as const);
+export const setAppStatus = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const);
+export const setInitializedSuccess = (value: boolean) => ({type: 'APP/SET-INITIALIZED', value} as const);
 
 // types
 export type SetAppErrorActionType = ReturnType<typeof setAppError>
 export type SetAppStatusActionType = ReturnType<typeof setAppStatus>
+export type SetAppInitializedActionType = ReturnType<typeof setInitializedSuccess>
 
 type ActionsType =
     | SetAppErrorActionType
     | SetAppStatusActionType
+    | SetAppInitializedActionType
 
-export type RequestStatusType =  'idle' | 'loading' | 'succeed' | 'failed'
+export type RequestStatusType = 'idle' | 'loading' | 'succeed' | 'failed';
+export type RequestErrorType = string | null
 
 export type InitialStateType = {
     status: RequestStatusType
-    isError: boolean
-
+    isError: RequestErrorType
+    initialized: boolean
 }
