371e9e413d9afd8ff04dbe2c87352d41ec34b849
pvl <pvlchkhrv@gmail.com>
2021-06-15 08:59:01 +0300

    fix: link to back

--- src/c1-main/m3-dal/cardsPackAPI.ts
+++ src/c1-main/m3-dal/cardsPackAPI.ts
@@ -31,7 +31,7 @@ export type PacksParamsType = {
 }
 
 const instance = axios.create({
-    baseURL: 'http://localhost:7542/2.0',
+    baseURL: 'https://neko-back.herokuapp.com/2.0',
     withCredentials: true
 });
 
